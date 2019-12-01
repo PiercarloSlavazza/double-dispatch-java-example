a1db7846238dee6490f9c87b056fa4da9795697a
Piercarlo Slavazza <p.slavazza@kie-services.com>
2019-12-01 08:28:46 +0100

    added method MessageVisitor#visit(MobilePushNotification)

--- src/main/java/com/java_tutorials/double_dispatch/messages/MessageVisitor.java
+++ src/main/java/com/java_tutorials/double_dispatch/messages/MessageVisitor.java
@@ -1,11 +1,13 @@
 package com.java_tutorials.double_dispatch.messages;
 
 import com.java_tutorials.double_dispatch.messages.impl.EmailMessage;
+import com.java_tutorials.double_dispatch.messages.impl.MobilePushNotification;
 import com.java_tutorials.double_dispatch.messages.impl.SMSMessage;
 
 public interface MessageVisitor<T> {
 
     T visit(SMSMessage smsMessage);
     T visit(EmailMessage emailMessage);
+    T visit(MobilePushNotification mobilePushNotification);
 
 }
